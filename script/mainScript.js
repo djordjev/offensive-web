262a22e67aa59a44419a79842f494a4e8f60a3c6
djordjev <djvukovic@outlook.com>
2014-03-02 23:46:49 +0100

    Embedded SWF

--- script/mainScript.js
+++ script/mainScript.js
@@ -3,13 +3,4 @@
  */
 
 function onMainPageLoad() {
-
-	var mainPageContent = document.getElementById('mainContentForSWF');
-	if (sessionStorage.getItem('facebookId')) {
-		getMe(function(response) {
-			mainPageContent.innerHTML = "Welcome " + response.name;
-		});
-	} else {
-		mainPageContent.innerHTML = "Welcome " + sessionStorage.getItem('username');
-	}
 }
